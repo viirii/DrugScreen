b9e1b3e8b07f530db919c82a988f62ae873b54ca
Qi Chu <g.cookie223@gmail.com>
2016-12-06 20:32:31 -0500

    rerun easy mod

--- active_most_prob_svm.sh
+++ active_most_prob_svm.sh
@@ -9,8 +9,8 @@ python3 active_most_proba_svm.py MODERATE 500 > log/AMP_MODERATE_500.log 2> log/
 python3 active_most_proba_svm.py MODERATE 1000 > log/AMP_MODERATE_1000.log 2> log/AMP_MODERATE_1000.err
 python3 active_most_proba_svm.py MODERATE 1500 > log/AMP_MODERATE_1500.log 2> log/AMP_MODERATE_1500.err
 python3 active_most_proba_svm.py MODERATE 2000 > log/AMP_MODERATE_2000.log 2> log/AMP_MODERATE_2000.err
-python3 active_most_proba_svm_difficult.py 0 > log/AMP_DIFFICULT_0.log 2> log/AMP_DIFFICULT_0.err
-python3 active_most_proba_svm_difficult.py 500 > log/AMP_DIFFICULT_500.log 2> log/AMP_DIFFICULT_500.err
-python3 active_most_proba_svm_difficult.py 1000 > log/AMP_DIFFICULT_1000.log 2> log/AMP_DIFFICULT_1000.err
-python3 active_most_proba_svm_difficult.py 1500 > log/AMP_DIFFICULT_1500.log 2> log/AMP_DIFFICULT_1500.err
-python3 active_most_proba_svm_difficult.py 2000 > log/AMP_DIFFICULT_2000.log 2> log/AMP_DIFFICULT_2000.err
+# python3 active_most_proba_svm_difficult.py 0 > log/AMP_DIFFICULT_0.log 2> log/AMP_DIFFICULT_0.err
+# python3 active_most_proba_svm_difficult.py 500 > log/AMP_DIFFICULT_500.log 2> log/AMP_DIFFICULT_500.err
+# python3 active_most_proba_svm_difficult.py 1000 > log/AMP_DIFFICULT_1000.log 2> log/AMP_DIFFICULT_1000.err
+# python3 active_most_proba_svm_difficult.py 1500 > log/AMP_DIFFICULT_1500.log 2> log/AMP_DIFFICULT_1500.err
+# python3 active_most_proba_svm_difficult.py 2000 > log/AMP_DIFFICULT_2000.log 2> log/AMP_DIFFICULT_2000.err
