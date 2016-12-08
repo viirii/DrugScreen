0f52177d2fa7106f87915baa041d0bd4341aa49d
Qi Chu <g.cookie223@gmail.com>
2016-12-08 00:25:08 -0500

    change name of logs

--- active_most_prob_random_forest.sh
+++ active_most_prob_random_forest.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-python3 active_most_proba_random_forest.py EASY 0 > log/AMP_EASY_0.log 2> log/AMP_EASY_0.err
-python3 active_most_proba_random_forest.py EASY 1000 > log/AMP_EASY_1000.log 2> log/AMP_EASY_1000.err
-python3 active_most_proba_random_forest.py EASY 1500 > log/AMP_EASY_1500.log 2> log/AMP_EASY_1500.err
-python3 active_most_proba_random_forest.py EASY 2000 > log/AMP_EASY_2000.log 2> log/AMP_EASY_2000.err
-python3 active_most_proba_random_forest.py MODERATE 0 > log/AMP_MODERATE_0.log 2> log/AMP_MODERATE_0.err
-python3 active_most_proba_random_forest.py MODERATE 500 > log/AMP_MODERATE_500.log 2> log/AMP_MODERATE_500.err
-python3 active_most_proba_random_forest.py MODERATE 1000 > log/AMP_MODERATE_1000.log 2> log/AMP_MODERATE_1000.err
-python3 active_most_proba_random_forest.py MODERATE 1500 > log/AMP_MODERATE_1500.log 2> log/AMP_MODERATE_1500.err
-python3 active_most_proba_random_forest.py MODERATE 2000 > log/AMP_MODERATE_2000.log 2> log/AMP_MODERATE_2000.err
-python3 active_most_proba_random_forest_difficult.py 0 > log/AMP_DIFFICULT_0.log 2> log/AMP_DIFFICULT_0.err
-python3 active_most_proba_random_forest_difficult.py 500 > log/AMP_DIFFICULT_500.log 2> log/AMP_DIFFICULT_500.err
-python3 active_most_proba_random_forest_difficult.py 1000 > log/AMP_DIFFICULT_1000.log 2> log/AMP_DIFFICULT_1000.err
-python3 active_most_proba_random_forest_difficult.py 1500 > log/AMP_DIFFICULT_1500.log 2> log/AMP_DIFFICULT_1500.err
-python3 active_most_proba_random_forest_difficult.py 2000 > log/AMP_DIFFICULT_2000.log 2> log/AMP_DIFFICULT_2000.err
+python3 active_most_proba_random_forest.py EASY 0 > log/AMRF_EASY_0.log 2> log/AMRF_EASY_0.err
+python3 active_most_proba_random_forest.py EASY 1000 > log/AMRF_EASY_1000.log 2> log/AMRF_EASY_1000.err
+python3 active_most_proba_random_forest.py EASY 1500 > log/AMRF_EASY_1500.log 2> log/AMRF_EASY_1500.err
+python3 active_most_proba_random_forest.py EASY 2000 > log/AMRF_EASY_2000.log 2> log/AMRF_EASY_2000.err
+python3 active_most_proba_random_forest.py MODERATE 0 > log/AMRF_MODERATE_0.log 2> log/AMRF_MODERATE_0.err
+python3 active_most_proba_random_forest.py MODERATE 500 > log/AMRF_MODERATE_500.log 2> log/AMRF_MODERATE_500.err
+python3 active_most_proba_random_forest.py MODERATE 1000 > log/AMRF_MODERATE_1000.log 2> log/AMRF_MODERATE_1000.err
+python3 active_most_proba_random_forest.py MODERATE 1500 > log/AMRF_MODERATE_1500.log 2> log/AMRF_MODERATE_1500.err
+python3 active_most_proba_random_forest.py MODERATE 2000 > log/AMRF_MODERATE_2000.log 2> log/AMRF_MODERATE_2000.err
+python3 active_most_proba_random_forest_difficult.py 0 > log/AMRF_DIFFICULT_0.log 2> log/AMRF_DIFFICULT_0.err
+python3 active_most_proba_random_forest_difficult.py 500 > log/AMRF_DIFFICULT_500.log 2> log/AMRF_DIFFICULT_500.err
+python3 active_most_proba_random_forest_difficult.py 1000 > log/AMRF_DIFFICULT_1000.log 2> log/AMRF_DIFFICULT_1000.err
+python3 active_most_proba_random_forest_difficult.py 1500 > log/AMRF_DIFFICULT_1500.log 2> log/AMRF_DIFFICULT_1500.err
+python3 active_most_proba_random_forest_difficult.py 2000 > log/AMRF_DIFFICULT_2000.log 2> log/AMRF_DIFFICULT_2000.err
